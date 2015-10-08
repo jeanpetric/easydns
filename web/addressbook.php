265ad990b52a317e5fa18c4c74202f76e92aa5bd
Jean Petric <petricomp@gmail.com>
2015-10-08 16:09:18 +0100

    fixed addressbook

--- web/addressbook.php
+++ web/addressbook.php
@@ -1,19 +1,24 @@
 <?php
 
 if (!filter_var($_POST['ip'], FILTER_VALIDATE_IP)) {
-  exit(1);
+    exit(1);
 } else {
-  $ip = $_POST['ip'];
-  $hash = $_POST['hash'];
-  $users = file("table.txt");
+    $ip = $_POST['ip'];
+    $domain = $_POST['domain'];
+    $hash = $_POST['hash'];
+    $fp = fopen('table.txt', 'r');
 
-  foreach ($users as $user)
-  {
-    $e = explode(" ", $user);
-    if ($e[1] == $hash) {
-      file_put_contents($e[0], $ip);
+    while ($line = fgets($fp))
+    {
+        $line = trim($line);
+        $e = explode(" ", $line);
+        if (!strcmp($e[0], $domain) && !strcmp($e[1], $hash)) {
+            file_put_contents($e[0], $ip);
+            break;
+        }
     }
-  }
+
+    fclose($fp);
 }
 
 ?>
